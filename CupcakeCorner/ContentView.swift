5968b7a2c8152bbafd4a6557a110226c44597317
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-30 12:40:36 -0600

    Validate and disable forms

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,35 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disabledForm: Bool {
+        username.isEmpty || username.count < 3 || email.isEmpty
+    }
+    
     var body: some View {
-        // this one won't apply the frame because SwiftUI won't know how big the image will be
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 4)
-//            .frame(width: 200, height: 200)
-        
-        // this one will apply the frame beacause SwiftUI (after the loading) will know how big the image will be
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 4) { image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder: {
-            // Display this while waiting for the image
-            ProgressView()
-        }
-        .frame(width: 200, height: 200)
-        
-        // this one will apply the frame beacause SwiftUI (after the loading) will know how big the image will be
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 4) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
+            
+            Button("Create Account") {
+                print("Creating Account...")
+            }
+            .disabled(disabledForm)
         }
-        .frame(width: 100, height: 100)
     }
 }
 
