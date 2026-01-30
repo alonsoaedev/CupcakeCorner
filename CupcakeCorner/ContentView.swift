db2440cb29f67acc85ca88d774ef477d41b85c2e
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-30 12:28:35 -0600

    Load an image from a remote server

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,48 +7,36 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, metadata) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-    }
-    
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
+        // this one won't apply the frame because SwiftUI won't know how big the image will be
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 4)
+//            .frame(width: 200, height: 200)
+        
+        // this one will apply the frame beacause SwiftUI (after the loading) will know how big the image will be
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 4) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            // Display this while waiting for the image
+            ProgressView()
+        }
+        .frame(width: 200, height: 200)
+        
+        // this one will apply the frame beacause SwiftUI (after the loading) will know how big the image will be
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 4) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            await loadData()
-        }
+        .frame(width: 100, height: 100)
     }
 }
 
