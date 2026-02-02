6a470916dc6587d05167169b01687fc32c87c791
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-02 17:18:42 -0600

    Check valid address

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Alonso Acosta on 02/02/26.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
